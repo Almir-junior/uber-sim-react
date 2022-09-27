2b266c897cb469b04263772a5d875dead6bb36b9
almir junior <almirpschera@hotmail.com>
2022-09-27 16:43:27 -0300

    login page and logout button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Amplify from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
 import config from './aws-exports';
 
@@ -15,7 +15,27 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+      </Authenticator>
     </div>
+  //   <Authenticator>
+  //   {({ signOut, user }) => (
+  //     <div className="App">
+  //       <p>
+  //         Hey {user.username}, welcome to my channel, with auth!
+  //       </p>
+  //       <button onClick={signOut}>Sign out</button>
+  //     </div>
+  //   )}
+  // </Authenticator>
   );
 }
 
