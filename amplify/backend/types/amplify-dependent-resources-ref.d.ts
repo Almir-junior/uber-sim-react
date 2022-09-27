f8e189893d1a10dde8ad0829f60586f3765ccdb5
almir junior <almirpschera@hotmail.com>
2022-09-26 22:13:37 -0300

    addes authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ubersimreact86586795": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from AWS</h1>
+        <h1>We now have Auth!</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
